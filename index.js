98e38e0ca6f0a25ca4081ac47a56953d87ddfe4d
patelsajan <patelsajan86@gmail.com>
2022-06-21 23:33:49 +0530

    check for key press

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+document.addEventListener('keypress', ev => {
+    console.log('keypress: ', ev);
+    if (ev.key === 'Enter') {
+
+    }
+});
+
 let index = Math.ceil(Math.random()*words_list.length);
 console.log('selected : ', index, ', out of : ', words_list.length);
 console.log('word is : ', words_list[index]);
+
 
