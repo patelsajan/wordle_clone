c9465d9be7e6d0234aeba414672b56475a987f5f
patelsajan <patelsajan86@gmail.com>
2022-06-23 23:24:19 +0530

    check for keypress

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+let word = '';
+let div1 = document.getElementById('div1');
+console.log(document);
+console.log(div1);
 document.addEventListener('keypress', ev => {
     console.log('keypress: ', ev);
     if (ev.key === 'Enter') {
-
+        console.log('check word');
+        checkWord();
+    } else {
+        div1.innerHTML = div1.innerHTML+ev.key;
     }
 });
 
@@ -9,4 +16,6 @@ let index = Math.ceil(Math.random()*words_list.length);
 console.log('selected : ', index, ', out of : ', words_list.length);
 console.log('word is : ', words_list[index]);
 
+function checkWord() {
 
+}
